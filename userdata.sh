ee426fa5aa478b9b5a4b78a1d9073592f40b4c10
Dawid Klocek <d.klocek@wp.pl>
2020-12-02 18:36:36 +0100

    userdata

--- userdata.sh
+++ userdata.sh
@@ -12,6 +12,8 @@ mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=
 apt-get install -y docker.io
 systemctl enable docker
 systemctl start docker
-docker run --name jenkins -p 8080:8080 -p 50000-50010:50000-50010 -v /efs/jenkins_home:/var/jenkins_home dklocek/jpd:v1 &
+docker run --name jenkins -p 8080:8080 -p 50000-50010:50000-50010 \
+  -v /var/run/docker.sock:/var/run/docker.sock -v /efs/jenkins_home:/var/jenkins_home \
+  dklocek/jpd:latest &
 docker update --restart=always jenkins
 docker start jenkins
